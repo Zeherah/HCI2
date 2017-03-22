7420772a5228e690c86fdeaba8aab9bddd153540
brettjewell <brettjewell11@gmail.com>
2017-03-21 21:43:21 -0500

    add grocery list menu icon

--- app/src/main/java/com/comp4020/listproject/GroceryListActivity.java
+++ app/src/main/java/com/comp4020/listproject/GroceryListActivity.java
@@ -3,8 +3,16 @@ package com.comp4020.listproject;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -17,7 +25,7 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.List;
 
-public class GroceryListActivity extends AppCompatActivity {
+public class GroceryListActivity extends AppCompatActivity{
 
     public static final String SELECTED_ITEMS = "com.comp4020.listproject.MESSAGE";
 
@@ -34,6 +42,7 @@ public class GroceryListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grocery_list);
+
 
         editItem = (EditText) findViewById(R.id.edit_item);
         addGroceryButton = (Button) findViewById(R.id.add_grocery_button);
@@ -95,7 +104,14 @@ public class GroceryListActivity extends AppCompatActivity {
             }
         });
 
+
+
+
+
     }
+
+
+
     public void itemDelete(final int position)
     {
         AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
